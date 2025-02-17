ec98e390943512d09e5944e1158271a701759dcd
Akimi Yano <akimi.mimi.yano@gmail.com>
2025-02-17 14:56:02 -0800

    search

--- epi_judge_python/search_first_key.py
+++ epi_judge_python/search_first_key.py
@@ -5,8 +5,19 @@ from test_framework import generic_test
 
 def search_first_of_k(A: List[int], k: int) -> int:
     # TODO - you fill in here.
-    return 0
-
+    
+    N = len(A)
+    left = 0
+    right = N-1
+    while left < right:
+        mid = (left + right) // 2
+        if A[mid] == k:
+            right = mid
+        elif A[mid] > k:
+            right = mid - 1
+        else:
+            left = mid + 1
+    return left if A and A[left] == k else -1
 
 if __name__ == '__main__':
     exit(
