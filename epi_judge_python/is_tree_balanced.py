53e819b0fa557b7aca3da17ea13e524428ae089e
Akimi Yano <akimi.mimi.yano@gmail.com>
2025-02-17 14:16:09 -0800

    tree

--- epi_judge_python/is_tree_balanced.py
+++ epi_judge_python/is_tree_balanced.py
@@ -4,8 +4,20 @@ from test_framework import generic_test
 
 def is_balanced_binary_tree(tree: BinaryTreeNode) -> bool:
     # TODO - you fill in here.
-    return True
-
+    
+    ans = True
+    def helper(cur):
+        nonlocal ans
+        if not cur:
+            return 0
+        left_height = helper(cur.left)
+        right_height = helper(cur.right)
+        if abs(left_height - right_height) > 1:
+            ans = False
+        return max(left_height, right_height) + 1
+    
+    helper(tree)
+    return ans
 
 if __name__ == '__main__':
     exit(
